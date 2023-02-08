6c1886bae20e23273d510d882674ddde57556c6e
Fernando Robles <fernandorobles97@yahoo.com>
2023-02-08 17:10:36 -0600

    Add array methods and explanations

--- arrays.js
+++ arrays.js
@@ -1,3 +1,14 @@
-var catNameIdeas = ["Morris","Tofu","Bagel","Ralph"]
-var coolNumbers = [97, 21, 55, 66]
-var booleanArray = [true, false, true, false]
+var catNameIdeas = ["Morris","Tofu","Bagel","Ralph"];
+var coolNumbers = [97, 21, 55, 66];
+var booleanArray = [true, false, true, false];
+
+// The pop method takes out the last element listed in an array.
+catNameIdeas.pop();
+
+// The unshift method adds on a element to the start of an array, 
+// and when ran, returns the new lenght of the array.
+console.log(coolNumbers.unshift(88));
+
+// The push method adds on a element to the end of an array, and when ran, 
+// returns the new lenght of the array.
+booleanArray.push(false);
