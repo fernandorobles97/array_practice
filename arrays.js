9e7f0f3a27f3b5f1c75f314232039593dc61eea8
Fernando Robles <fernandorobles97@yahoo.com>
2023-02-08 17:20:48 -0600

    Add thoughts on index positions

--- arrays.js
+++ arrays.js
@@ -12,3 +12,17 @@ console.log(coolNumbers.unshift(88));
 // The push method adds on a element to the end of an array, and when ran, 
 // returns the new lenght of the array.
 booleanArray.push(false);
+
+
+// Index position describes the number an element is an array. A common misconception
+// would be that the first element is 1, when it is actually 0. I will call upon some 
+// elements from the arrays created above and predict the output.
+
+console.log(catNameIdeas[1]);
+// expected output: Tofu
+
+console.log(coolNumbers[0]);
+// expected output: 88; this number was added on from the unshift method
+
+console.log(booleanArray[0]);
+// expected output: true
